1c51cc03d5a7a0a74d76f7c8516f57daeefaccd6
Alexander Plavin <alexander@plav.in>
2025-12-09 00:38:28 +0000

    basic support

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,15 @@ using TestItems
 using TestItemRunner
 @run_package_tests
 
+@testitem "example gaia" begin
+    using StructArrays
+
+    tbl = EnhancedCSV.read(StructArray, "data/gaia.csv")
+    @test length(tbl) == 5
+    @test propertynames(tbl)[1] == :solution_id
+    @test all(==(375316653866487564), tbl.solution_id)
+    @test tbl.n_transits == [25, 21, 23, 26, 25]
+end
 
 @testitem "_" begin
     import Aqua
